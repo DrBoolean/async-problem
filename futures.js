3c2f8cc12614bdf706ad8496491bdad4bc230e6a
Brian Lonsdorf <brian@looprecur.com>
2015-09-19 15:37:47 -0700

    use compose and traverse

--- futures.js
+++ futures.js
@@ -6,42 +6,40 @@ const path = require('path');
 const Future = require('data.task');
 const R = require('ramda');
 
-
 // join :: String -> String -> String
 const join = R.curryN(2, path.join);
 
 // data Text = Buffer | String
 // readFile :: String -> String -> Future Error Text
 const readFile = R.curry((encoding, filename) =>
-  new Future((rej, res) => {
-    fs.readFile(filename, {encoding: encoding}, (err, data) => {
-      if (err != null) {
-        rej(err);
-      } else {
-        res(data);
-      }
-    });
-  })
-);
+  new Future((rej, res) =>
+    fs.readFile(filename, encoding, (e, data) => e ? rej(e) : res(data))
+  )
+)
+
+// traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
+const traverse = f => R.compose(R.commute(Future.of), R.map(f))
+
+
+// parseAndReadFiles :: String -> String -> Future Error String
+const parseAndReadFiles = dir => R.compose( R.map(R.join(''))
+                                          , traverse(readFile('utf8'))
+                                          , R.map(join(dir))
+                                          , R.match(/^.*(?=\n)/gm)
+                                          )
 
 // concatFiles :: String -> Future Error String
-const concatFiles = dir =>
-  R.pipe(
-    readFile('utf8'),                 // :: Future Error String
-    R.map(R.match(/^.*(?=\n)/gm)),    // :: Future Error [String]
-    R.map(R.map(join(dir))),          // :: Future Error [String]
-    R.map(R.map(readFile('utf8'))),   // :: Future Error [Future Error String]
-    R.chain(R.commute(Future.of)),    // :: Future Error [String]
-    R.map(R.join(''))                 // :: Future Error String
-  )(join(dir, 'index.txt'));
+const concatFiles = dir => R.compose( R.chain(parseAndReadFiles(dir))
+                                    , readFile('utf8')
+                                    , join(dir)
+                                    )('/index.txt')
 
 // write :: Object -> * -> *
-const write = R.flip(R.invoker(1, 'write'));
-
+const write = R.flip(R.invoker(1, 'write'))
 
 const main = () => {
   concatFiles(process.argv[2])
-  .fork(write(process.stderr), write(process.stdout));
-};
+  .fork(write(process.stderr), write(process.stdout))
+}
 
-if (process.argv[1] === __filename) main();
+if (process.argv[1] === __filename) main()
